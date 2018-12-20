9f86cc02f27d6b49578dd994d9cd08a8ea6bdd14
Santiago Cardenas Valderrama <santiago.cardenas@globant.com>
2018-12-20 08:49:51 -0500

    first commit

---
+++ src/main/java/com/electroeing/CargoTomcatDeploy/CargoTomcatDeployApplication.java
@@ -0,0 +1,20 @@
+package com.electroeing.CargoTomcatDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CargoTomcatDeployApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoTomcatDeployApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CargoTomcatDeployApplication.class);
+	}
+}
+
